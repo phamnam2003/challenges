2368ef91c11ff6f484dc47bed5126faddc9b7fa5
phamnam2003 <namphamhai7@gmail.com>
2025-05-12 17:03:37 +0700

    feat(concurrency): use select to multiple go routine

---
+++ concurrency/select/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"log"
+	"time"
+)
+
+type Message struct {
+	OrderID string
+	Title   string
+	Price   int
+}
+
+func OrderProduct(buyChan chan<- Message, orders []Message) {
+	for _, order := range orders {
+		log.Printf("[ORDER_TASK_PROCESSING]: %+v", order)
+		time.Sleep(1 * time.Second)
+		buyChan <- order
+	}
+	close(buyChan)
+}
+
+func CancelOrder(cancelChan chan<- string, cancelOrders []string) {
+	for _, cancelOrder := range cancelOrders {
+		log.Printf("[CANCEL_ORDER_TASK_PROCESSING]: %+v", cancelOrders)
+		time.Sleep(5 * time.Second)
+		cancelChan <- cancelOrder
+	}
+	close(cancelChan)
+}
+
+func HandlerOrder(buyChan <-chan Message, cancelChan <-chan string) {
+	for {
+		select {
+		case buy := <-buyChan:
+			{
+			}
+		case cancel := <-cancelChan:
+			{
+			}
+		}
+	}
+}
+
+func main() {
+	buyChan := make(chan Message)
+	cancelChan := make(chan string)
+
+	// mock concurency order requests
+	orders := []Message{
+		{OrderID: "order-001", Title: "Book", Price: 100},
+		{OrderID: "order-002", Title: "Toy", Price: 200},
+		{OrderID: "order-003", Title: "Table", Price: 300},
+		{OrderID: "order-004", Title: "Clothe", Price: 400},
+		{OrderID: "order-005", Title: "Glasses", Price: 500},
+	}
+
+	// mock concurency cancel order requests
+	cancelOrders := []string{"order-002", "order-003", "order-005"}
+	go OrderProduct(buyChan, orders)
+	go CancelOrder(cancelChan, cancelOrders)
+	go HandlerOrder(buyChan, cancelChan)
+
+	time.Sleep(10 * time.Second)
+}
