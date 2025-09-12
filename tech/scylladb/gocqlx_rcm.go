1e7068b433377114b4fc3fa365dacc48eedaf9e3
phamnam2003 <namphamhai7@gmail.com>
2025-09-12 15:21:38 +0700

    refactor(cqlx): use models generated from schemagen into query

--- tech/scylladb/gocqlx_rcm.go
+++ tech/scylladb/gocqlx_rcm.go
@@ -4,9 +4,9 @@ import (
 	"log"
 
 	"github.com/gocql/gocql"
+	"github.com/phamnam2003/challenges/tech/scylladb/models"
 	sccore "github.com/phamnam2003/challenges/tech/scylladb/sc_core"
 	"github.com/scylladb/gocqlx/v2"
-	"github.com/scylladb/gocqlx/v2/table"
 )
 
 type Mutant struct {
@@ -27,16 +27,7 @@ func main() {
 
 	log.Println("connected to scylladb")
 
-	// create table metadata
-	mutantMetadata := table.Metadata{
-		Name:    "mutant_data",
-		Columns: []string{"first_name", "last_name", "address", "picture_location"},
-		PartKey: []string{"first_name", "last_name"},
-		SortKey: []string{},
-	}
-	mutantTable := table.New(mutantMetadata)
-
-	m := Mutant{
+	m := models.MutantDataStruct{
 		FirstName:       "Pham",
 		LastName:        "Nam",
 		Address:         "ThaiBinh, Vietnam",
@@ -44,40 +35,40 @@ func main() {
 	}
 
 	// insert row into table
-	q := session.Query(mutantTable.Insert()).BindStruct(m)
+	q := session.Query(models.MutantData.Insert()).BindStruct(m)
 	if err := q.ExecRelease(); err != nil {
 		log.Fatal("unable to insert mutant data", err)
 	}
 
 	// get one row from table with filter call bind struct filter
-	q = session.Query(mutantTable.Get()).BindStruct(m)
+	q = session.Query(models.MutantData.Get()).BindStruct(m)
 	if err := q.GetRelease(&m); err != nil {
 		log.Printf("unable to get mutant data: %s", err)
 	}
 	log.Printf("get one mutant_data: %+v", m)
 
-	q = session.Query(mutantTable.Delete()).BindStruct(m)
+	q = session.Query(models.MutantData.Delete()).BindStruct(m)
 	if err := q.ExecRelease(); err != nil {
 		log.Fatal("unable to delete mutant data", err)
 	}
 
-	q = session.Query(mutantTable.Get()).BindStruct(m)
+	q = session.Query(models.MutantData.Get()).BindStruct(m)
 	if err := q.GetRelease(&m); err != nil {
 		log.Printf("error get mutant data: %v %v", err, gocql.ErrNotFound)
 	}
 
 	// insert record into table mutant_data serve update query
-	q = session.Query(mutantTable.Insert()).BindStruct(m)
+	q = session.Query(models.MutantData.Insert()).BindStruct(m)
 	if err := q.ExecRelease(); err != nil {
 		log.Fatal("unable to insert mutant data", err)
 	}
 
-	mU := Mutant{
+	mU := models.MutantDataStruct{
 		FirstName: "Pham",
 		LastName:  "Nam",
 		Address:   "HungYen, Vietnam",
 	}
-	q = session.Query(mutantTable.Update("address")).BindStruct(mU)
+	q = session.Query(models.MutantData.Update("address")).BindStruct(mU)
 	if err := q.ExecRelease(); err != nil {
 		log.Fatalf("error update: %s", err)
 	}
