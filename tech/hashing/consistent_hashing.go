de957da10adf0f54fc1c6d6e82f419296a62ac54
phamnam2003 <namphamhai7@gmail.com>
2025-09-19 13:50:56 +0700

    feat(hashing): imple consistent hashing with virtual nodes

--- tech/hashing/consistent_hashing.go
+++ tech/hashing/consistent_hashing.go
@@ -2,6 +2,7 @@
 package hashing
 
 import (
+	"fmt"
 	"hash/fnv"
 	"sort"
 )
@@ -10,48 +11,51 @@ import (
 type ServerNode struct {
 	ID   string
 	Hash uint32
-
-	// can be load instance the DB connection
 }
 
 // HashRing represents the consistent hashing ring.
 type HashRing struct {
-	nodes []ServerNode
+	nodes    []ServerNode
+	replicas int
 }
 
-// hashKey computes the FNV-1a hash of a given key [can use another hash func, like sha-1, ...].
+// hashKey computes the FNV-1a hash of a given key.
 func hashKey(key string) uint32 {
-	hash := fnv.New32a() // init fnv hasher 32-bit
+	hash := fnv.New32a()
 	hash.Write([]byte(key))
 
 	return hash.Sum32()
 }
 
-// AddNode adds a new server node to the hash ring.
-func (h *HashRing) AddNode(id string) {
-	n := ServerNode{ID: id, Hash: hashKey(id)}
-	h.nodes = append(h.nodes, n)
-	sort.Slice(h.nodes, func(i, j int) bool {
-		return h.nodes[i].Hash < h.nodes[j].Hash
-	})
-}
-
-// NewHashRing creates a new HashRing with the given server nodes.
-func NewHashRing(servers []string) *HashRing {
-	ring := &HashRing{}
+// NewHashRing creates a new HashRing with the given server nodes and replicas.
+func NewHashRing(servers []string, replicas int) *HashRing {
+	ring := &HashRing{replicas: replicas}
 	for _, s := range servers {
 		ring.AddNode(s)
 	}
 	return ring
 }
 
+// AddNode adds a new server node to the hash ring with virtual nodes.
+func (h *HashRing) AddNode(id string) {
+	for i := 0; i < h.replicas; i++ {
+		vnodeID := fmt.Sprintf("%s#%d", id, i)
+		n := ServerNode{ID: id, Hash: hashKey(vnodeID)}
+		h.nodes = append(h.nodes, n)
+	}
+	sort.Slice(h.nodes, func(i, j int) bool {
+		return h.nodes[i].Hash < h.nodes[j].Hash
+	})
+}
+
+// GetNode finds the server node responsible for a given key.
 func (h *HashRing) GetNode(key string) string {
 	if len(h.nodes) == 0 {
 		return ""
 	}
 	kh := hashKey(key)
 
-	// binary search return nearest node by the clockwise direction
+	// binary search return nearest node by clockwise direction
 	idx := sort.Search(len(h.nodes), func(i int) bool {
 		return h.nodes[i].Hash >= kh
 	})
