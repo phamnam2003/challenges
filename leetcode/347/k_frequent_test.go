bec86550d0ef2b3ae07db075ced043a2faa9ab6c
phamnam2003 <namphamhai7@gmail.com>
2025-09-22 06:13:28 +0700

    test(leetcode): kfreq bigest testing leetcode

--- leetcode/347/k_frequent_test.go
+++ leetcode/347/k_frequent_test.go
@@ -13,7 +13,23 @@ func TestTopKFrequent(t *testing.T) {
 		array []int
 		k     int
 		res   []int
-	}{}
+	}{
+		{
+			array: []int{1, 1, 1, 2, 2, 3},
+			k:     2,
+			res:   []int{1, 2},
+		},
+		{
+			array: []int{1},
+			k:     1,
+			res:   []int{1},
+		},
+		{
+			array: []int{4, 1, -1, 2, -1, 2, 3},
+			k:     2,
+			res:   []int{-1, 2},
+		},
+	}
 	for _, c := range tCases {
 		t.Run("k_freq", func(t *testing.T) {
 			t.Parallel()
