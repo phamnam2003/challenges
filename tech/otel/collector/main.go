d5b8f507208471131a4999acf0b98198d80403b4
phamnam2003 <namphamhai7@gmail.com>
2025-10-20 09:03:36 +0700

    feat(error): wrap error handling with conflict params and merge resource default resource, avoid missing para,s

--- tech/otel/collector/main.go
+++ tech/otel/collector/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
@@ -36,12 +37,12 @@ func newTraceProvider(ctx context.Context) (*sdktrace.TracerProvider, error) {
 
 	// res: resource in opentelemetry
 	res, err := resource.New(ctx,
-		resource.WithFromEnv(),
-		resource.WithOS(),
-		resource.WithContainer(),
-		resource.WithProcess(),
-		resource.WithTelemetrySDK(),
-		resource.WithHost(),
+		resource.WithFromEnv(),      // Discover and provide attributes from OTEL_RESOURCE_ATTRIBUTES and OTEL_SERVICE_NAME environment variables.
+		resource.WithTelemetrySDK(), // Discover and provide information about the OpenTelemetry SDK used.
+		resource.WithProcess(),      // Discover and provide process information.
+		resource.WithOS(),           // Discover and provide OS information.
+		resource.WithContainer(),    // Discover and provide container information.
+		resource.WithHost(),         // Discover and provide host information.
 		resource.WithAttributes(
 			semconv.ServiceName("otel-http-demo"),
 			semconv.ServiceVersion("1.0.0"),
@@ -50,7 +51,18 @@ func newTraceProvider(ctx context.Context) (*sdktrace.TracerProvider, error) {
 		),
 	)
 	if err != nil {
+		if errors.Is(err, resource.ErrPartialResource) || errors.Is(err, resource.ErrSchemaURLConflict) {
+			log.Printf("warning: partial resource created: %v", err)
+		}
 		return nil, fmt.Errorf("failed to create resource: %w", err)
+	}
+
+	res, err = resource.Merge(resource.Default(), res)
+	if err != nil {
+		if errors.Is(err, resource.ErrPartialResource) || errors.Is(err, resource.ErrSchemaURLConflict) {
+			log.Printf("warning: partial resource merged: %v", err)
+		}
+		return nil, fmt.Errorf("failed to merged resource: %w", err)
 	}
 
 	tp := sdktrace.NewTracerProvider(
