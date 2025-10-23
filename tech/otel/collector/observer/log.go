8eb62bc17b46a819575929c7a685499e769e51d6
phamnam2003 <namphamhai7@gmail.com>
2025-10-24 04:53:03 +0700

    feat(logs): config bridges otelzap logger push log into collector, collector push to loki logs

--- tech/otel/collector/observer/log.go
+++ tech/otel/collector/observer/log.go
@@ -3,14 +3,27 @@ package observer
 import (
 	"context"
 	"fmt"
+	"io"
+	"os"
+	"sync"
 	"time"
 
+	"go.opentelemetry.io/contrib/bridges/otelzap"
 	"go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploggrpc"
 	sdklog "go.opentelemetry.io/otel/sdk/log"
 	"go.opentelemetry.io/otel/sdk/resource"
+	semconv "go.opentelemetry.io/otel/semconv/v1.37.0"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 	"google.golang.org/grpc"
 )
 
+var (
+	zapOTel *zap.Logger
+	once    sync.Once
+)
+
+// newLoggerProvider creates a new OpenTelemetry LoggerProvider that exports logs
 func newLoggerProvider(ctx context.Context, conn *grpc.ClientConn, res *resource.Resource) (*sdklog.LoggerProvider, error) {
 	exporter, err := otlploggrpc.New(ctx, otlploggrpc.WithGRPCConn(conn), otlploggrpc.WithRetry(otlploggrpc.RetryConfig{
 		Enabled:         true,
@@ -27,5 +40,22 @@ func newLoggerProvider(ctx context.Context, conn *grpc.ClientConn, res *resource
 		sdklog.WithProcessor(batchProcessor),
 		sdklog.WithResource(res),
 	)
+	setZapOTel(zapcore.InfoLevel, os.Stdout, lp)
 	return lp, nil
+}
+
+// setZapOTel sets up a zap logger that writes to the provided writer and uses the given LoggerProvider
+func setZapOTel(level zapcore.Level, w io.Writer, lp *sdklog.LoggerProvider) {
+	once.Do(func() {
+		core := zapcore.NewTee(
+			zapcore.NewCore(zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig()), zapcore.AddSync(w), level),
+			otelzap.NewCore("observer/otelzap/bridges", otelzap.WithSchemaURL(semconv.SchemaURL), otelzap.WithLoggerProvider(lp)),
+		)
+		zapOTel = zap.New(core, zap.AddCaller())
+	})
+}
+
+// GetZapOTel returns the singleton zap logger configured for OpenTelemetry
+func GetZapOTel() *zap.Logger {
+	return zapOTel
 }
