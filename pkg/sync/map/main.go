a608af7395f789bb8f598947455be60071c74dfd
phamnam2003 <namphamhai7@gmail.com>
2026-01-28 17:32:18 +0700

    feat(map): use api of sync.Map

--- pkg/sync/map/main.go
+++ pkg/sync/map/main.go
@@ -27,4 +27,23 @@ func main() {
 	sMap.Store("key", "value")
 	deleted := sMap.CompareAndDelete("key", "value")
 	log.Println("deleted: ", deleted)
+
+	for i := range 10 {
+		sMap.Store(i, i*i)
+	}
+
+	sMap.Range(func(key, value any) bool {
+		log.Printf("key: %v, value: %v\n", key, value)
+
+		// return true to continue iteration loop
+		return true
+	})
+
+	prev, loaded := sMap.Swap(1, 123)
+	log.Printf("previous value: %v, loaded: %v\n", prev, loaded)
+	prev, loaded = sMap.Swap(2, 4)
+	log.Printf("previous value: %v, loaded: %v\n", prev, loaded)
+
+	val, loaded := sMap.LoadAndDelete(1)
+	log.Printf("value: %v, loaded: %v\n", val, loaded)
 }
