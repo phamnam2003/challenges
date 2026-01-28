f4014f81e9d7859d7d2577e4302b2ce6f5643f73
phamnam2003 <namphamhai7@gmail.com>
2026-01-28 16:32:27 +0700

    chore(docs): add comment explain consume runnin g

--- pkg/sync/cond/main.go
+++ pkg/sync/cond/main.go
@@ -11,6 +11,7 @@ import (
 	"time"
 )
 
+// Queue represents a thread-safe queue with condition variable support.
 type Queue struct {
 	items  []string
 	lock   sync.Locker
@@ -18,6 +19,7 @@ type Queue struct {
 	closed bool
 }
 
+// newQ initializes and returns a new Queue instance.
 func newQ() *Queue {
 	lock := &sync.Mutex{}
 	return &Queue{
@@ -27,20 +29,27 @@ func newQ() *Queue {
 	}
 }
 
+// Consume starts consuming items from the queue using the specified number of goroutines.
 func (q *Queue) Consume(size int) {
+	// create go routines with limit size, but this is not recommended for production use
+	// because it may lead to resource exhaustion.
 	for range size {
 		go func() {
+			// loop infinitely to consume items
 			for {
 				q.lock.Lock()
 				for len(q.items) == 0 && !q.closed {
 					log.Println("consume sleeping")
+					// move go routine to g-parking state with empty queue and not closed
 					q.cond.Wait()
 				}
 				if q.closed && len(q.items) == 0 {
 					q.lock.Unlock()
 					return
 				}
+				// get message from queue
 				item := q.items[0]
+				// assign new slice to queue items
 				q.items = q.items[1:]
 				q.lock.Unlock()
 
@@ -51,6 +60,7 @@ func (q *Queue) Consume(size int) {
 	}
 }
 
+// Enqueue adds an item to the queue and signals a waiting consumer.
 func (q *Queue) Enqueue(item string) {
 	q.lock.Lock()
 	defer q.lock.Unlock()
@@ -58,13 +68,18 @@ func (q *Queue) Enqueue(item string) {
 		return
 	}
 	q.items = append(q.items, item)
+
+	// signal one consumer to handle message
 	q.cond.Signal()
 }
 
+// Close marks the queue as closed and broadcasts to all waiting consumers.
 func (q *Queue) Close() {
 	q.lock.Lock()
 	defer q.lock.Unlock()
 	q.closed = true
+
+	// wake up all consumers to shutdown go routines
 	q.cond.Broadcast()
 }
 
